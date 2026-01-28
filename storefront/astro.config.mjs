57737c14838515d110301e8f9594dfa9f3b91b9c
kashipu <graficowm@gmail.com>
2026-01-27 21:52:18 -0500

    feat: config ssr astro, adapter node y dockerfile para dokploy

--- storefront/astro.config.mjs
+++ storefront/astro.config.mjs
@@ -4,9 +4,17 @@ import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
+  server: {
+    host: true
+  },
   integrations: [react(), sitemap()],
 
   vite: {
